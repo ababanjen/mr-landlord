e010dca6bd1d3a084b857dfe9141a3a8d32e4890
Jen <jenelyn.ababan@chromedia.com>
2023-06-29 20:16:14 +0800

    adding sample content

--- components/Organisms/General/index.tsx
+++ components/Organisms/General/index.tsx
@@ -1,10 +1,10 @@
 import Text from "@/components/Atoms/Text";
 const General = () => {
-    return(
-        <div className="flex flex-col">
-            <Text>General contents here</Text>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col">
+      <Text>General contents here</Text>
+    </div>
+  );
+};
 
 export default General;
