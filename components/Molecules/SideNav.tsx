65093ad56bde84c383c90593067a69d17c15e635
Jen <jenelyn.ababan@chromedia.com>
2023-06-29 21:17:28 +0800

    adjustments

--- components/Molecules/SideNav.tsx
+++ components/Molecules/SideNav.tsx
@@ -1,20 +1,20 @@
-"use client";
-import { Sidenav } from "tw-elements";
-import { useRef } from "react";
-import useInitTE from "@/hooks/global/useInitTE";
-import Text from "@/components/Atoms/Text";
-import useStore from "@/hooks/global/useStore";
+'use client';
+import { Sidenav } from 'tw-elements';
+import { useRef } from 'react';
+import useInitTE from '@/hooks/global/useInitTE';
+import Text from '@/components/Atoms/Text';
+import useStore from '@/hooks/global/useStore';
 
 const SideNav = () => {
   const sideNavRef = useRef(null);
-  useInitTE({ Sidenav }, sideNavRef, { sidenavPosition: "unset" });
+  useInitTE({ Sidenav }, sideNavRef, { sidenavWidth: 244 });
 
-  const setSidenav = useStore((state) => state.setSidenav, ["admin"]);
+  const setSidenav = useStore((state) => state.setSidenav, ['admin']);
 
   const onClickSidenav = (data: string) => setSidenav(data);
 
   return (
-    <div className="min-h-screen h-auto w-auto">
+    <div className="min-h-screen h-auto relative w-[18%]">
       <nav
         id="sidenav-1"
         className="absolute left-0 top-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800"
@@ -50,4 +50,4 @@ const SideNav = () => {
 
 export default SideNav;
 
-export const sidenavLinks = ["dashboard", "general", "collections", "users"];
+export const sidenavLinks = ['dashboard', 'general', 'collections', 'users'];
