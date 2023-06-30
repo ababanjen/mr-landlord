ccc49ba8c91914b3a8258177d857f4840cd12d24
Jen <jenelyn.ababan@chromedia.com>
2023-06-30 19:45:57 +0800

    form setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx, html}',
     './components/**/*.{js,ts,jsx,tsx,mdx, html}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/tw-elements/dist/js/**/*.js',
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {
@@ -13,10 +13,10 @@ module.exports = {
         warning: '#ef9eae',
         success: '#a0eee8',
         primary: { 500: '#6ba8f5', 600: '#528ed8' },
-        label: '#747474',
-      },
-    },
+        label: '#747474'
+      }
+    }
   },
   darkMode: 'class',
-  plugins: [require('tw-elements/dist/plugin.cjs')],
-};
+  plugins: [require('tw-elements/dist/plugin.cjs')]
+}
