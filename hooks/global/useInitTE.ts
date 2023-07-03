a422968901974906ecefa027050ab32329a6ff0f
Jen <jenelyn.ababan@chromedia.com>
2023-07-03 16:24:57 +0800

    update

--- hooks/global/useInitTE.ts
+++ hooks/global/useInitTE.ts
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable security/detect-object-injection */
 import { useEffect } from "react";
 import { keys, lowerCase } from "lodash";
